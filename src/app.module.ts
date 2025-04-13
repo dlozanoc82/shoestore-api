66ba7502c0ea6ce547ff5b6d5882553c4c9f169f
Daniel-WSL <dlozanoc862@gmail.com>
2025-04-13 17:04:30 -0500

    Use Factory para el manejode promesas y llamados a API

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,15 @@ import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { BrandsModule } from './brands/brands.module';
 import { CustomerModule } from './customer/customer.module';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 const API_KEY = '123455656';
 const API_KEY_PROD = 'PROD-123455656';
 
 @Module({
   imports: [
+    HttpModule,
     UsersModule,
     ProductsModule,
     CategoriesModule,
@@ -24,6 +27,17 @@ const API_KEY_PROD = 'PROD-123455656';
     {
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'production' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (httpService: HttpService) => {
+        const request = httpService.get(
+          'https://jsonplaceholder.typicode.com/todos',
+        );
+        const tasks = await lastValueFrom(request);
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,11 @@ export class AppService {
   constructor(
     @Inject('API_KEY')
     private apiKey: string,
+    @Inject('TASKS')
+    private tasks: any[], // Assuming tasks is an array of any type
   ) {}
   getHello(): string {
+    console.log(this.tasks);
     return `Hello World! ${this.apiKey}`;
   }
 }
