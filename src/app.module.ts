02fb2e5369b31d6e988242fe3d3847243aef2af0
Daniel-WSL <dlozanoc862@gmail.com>
2025-04-13 14:25:55 -0500

    Se crea la estructura principal para el backend store de zapatillas

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,31 @@ import { AppService } from './app.service';
 import { ProductsController } from './controllers/products/products.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductService } from './services/product/product.service';
+import { BrandsController } from './controllers/brands/brands.controller';
+import { CustomerController } from './controllers/customer/customer.controller';
+import { UsersController } from './controllers/users/users.controller';
+import { UsersService } from './services/users/users.service';
+import { BrandsService } from './services/brands/brands.service';
+import { CustomerService } from './services/customer/customer.service';
+import { CategoriesService } from './services/categories/categories.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductService],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    BrandsController,
+    CustomerController,
+    UsersController,
+  ],
+  providers: [
+    AppService,
+    ProductService,
+    UsersService,
+    BrandsService,
+    CustomerService,
+    CategoriesService,
+  ],
 })
 export class AppModule {}
